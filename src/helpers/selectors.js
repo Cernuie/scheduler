3920dc1602a9a55244316791e587ff1838610a43
Alexander Nguyen <cernvii@gmail.com>
2021-02-18 17:31:59 -0500

    getinterview helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,3 +13,16 @@ export function getAppointmentsForDay(state, day) {
 
 }
 
+export function getInterview(state, interview) {
+  if (interview === null) {
+    return null;
+  } else {
+    const interviewerId = interview.interviewer;
+    let interviewObj = {
+      "student": interview.student,
+      "interviewer": state.interviewers[interviewerId],
+    }
+    return interviewObj
+  }
+}
+
